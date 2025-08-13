645295519cadd2784cb4012d9538f9eae19fa597
Tomás <tomaspitavino@gmail.com>
2025-08-13 14:04:26 -0300

    prueba de react

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+<body>
+    <h1>Muebles a tu alcance</h1>
+    <p>Este es tu lugar para decorar tu casa</p>
+    <input type="button" value="Ingresá acá"></input>
+
+</body>
+    <h2>Exibidor</h2>
+    <ul> 
+        <li><strong>Muebles</strong></li>
+        <li><strong>Escritorios</strong></li>
+        <li><strong>Sillones</strong></li>
+        <li><strong>Pequeños Muebles</strong></li>
+    </ul>
     </>
   )
 }
