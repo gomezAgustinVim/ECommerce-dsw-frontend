dd0a97af3bebad63a6da0c1447f3096e5f77567d
Tomás <tomaspitavino@gmail.com>
2025-08-13 21:41:45 -0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-<body>
-    <h1>Muebles a tu alcance</h1>
-    <p>Este es tu lugar para decorar tu casa</p>
-    <input type="button" value="Ingresá acá"></input>
-
-</body>
-    <h2>Exibidor</h2>
-    <ul> 
-        <li><strong>Muebles</strong></li>
-        <li><strong>Escritorios</strong></li>
-        <li><strong>Sillones</strong></li>
-        <li><strong>Pequeños Muebles</strong></li>
-    </ul>
-    </>
-  )
+    <div className="header-container">
+      <div className="header-box">
+        <h1>Muebles a tu alcance</h1>
+      </div>
+      <body>
+        <p>Este es tu lugar para decorar tu casa</p>
+        <input type="button" value="Ingresá acá"></input>
+      </body>
+      <h1>Exibidor</h1>
+      <ul>
+        <li>
+          <strong>Muebles</strong>
+        </li>
+        <li>
+          <strong>Sillones</strong>
+        </li>
+        <li>
+          <strong>Pequeños Muebles</strong>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
