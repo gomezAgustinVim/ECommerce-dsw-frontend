fdf4f6fae0155ad153de3c63cb42b2ca1e6cdd2f
Tomás <tomaspitavino@gmail.com>
2025-08-14 08:55:48 -0300

    commit1

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,18 @@ function App() {
       <div className="header-box">
         <h1>Muebles a tu alcance</h1>
       </div>
-      <body>
+      <div className="top-right-box">
         <p>Este es tu lugar para decorar tu casa</p>
-        <input type="button" value="Ingresá acá"></input>
-      </body>
+        <input type="button" value="Ingresá acá" />
+      </div>
       <h2>Exibidor</h2>
       <ul>
         <li>
           <strong>Muebles</strong>
         </li>
-
         <li>
           <strong>Sillones</strong>
         </li>
-        
         <li>
           <strong>Pequeños Muebles</strong>
         </li>
