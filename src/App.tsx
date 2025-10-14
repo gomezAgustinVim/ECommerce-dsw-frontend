fc6a5115ad3319f19804bbe23cf07d27ac5f124c
lordagustin22 <niconecrofilo589@gmail.com>
2025-10-13 21:31:23 -0300

    Conectado el front con back (beta)

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,21 @@ import Header from './components/header';
 import Home from './pages/home';
 import Carrito from './pages/carrito'; // <-- importar el componente
 import Login from './components/login';
+import { type Cliente } from './types';
+import { useEffect, useState } from "react";
 function App() {
+    const [clientes, setClientes] = useState<Cliente[]>([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/api/clientes") // 👈 URL del backend
+            .then((res) => res.json())
+            .then((json) => {
+                console.log(json); // <- verificás qué llega
+                setClientes(json.data); // <- extrae el array "data"
+            })
+            .catch((err) => console.error("Error al conectar al backend:", err));
+    }, []);
+
     return (
         <div className="bg-frame">
             <Header />
@@ -13,6 +27,16 @@ function App() {
                 <Route path="/carrito" element={<Carrito />} /> {/* <-- ruta para el carrito */}
                 <Route path="/login" element={<Login />} />
             </Routes>
+
+            <h1>Clientes</h1>
+            {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+            {/* ejemplo: mostrar nombre y correo */}
+            {clientes.map((cliente) => (
+                <div key={cliente.id}>
+                    <h2>{cliente.nombre} {cliente.apellido}</h2>
+                    <p>{cliente.email}</p>
+                </div>
+            ))}
         </div>
     );
 }
--- src/types.tsx
+++ src/types.tsx
@@ -1,25 +1,41 @@
 export type Mueble = {
-  id: number;
-  descripcion: string;
-  etiqueta: string;
-  stock: number;
-  precioUnitario: number;
-  imagenes?: string[];
+    id: number;
+    descripcion: string;
+    etiqueta: string;
+    stock: number;
+    precioUnitario: number;
+    imagenes?: string[];
 };
 
 export type Item = {
-  id: number;
-  cantidad: number;
-  subtotal: number;
-  estado: string;
-  mueble: Mueble;
+    id: number;
+    cantidad: number;
+    subtotal: number;
+    estado: string;
+    mueble: Mueble;
 };
 
 export type Pedido = {
-  id: number;
-  estado: string;
-  total: number;
-  lineas: LineaPedido[];
-  cliente: { id: number; nombre: string };
-  fechaHora: string;
+    id: number;
+    estado: string;
+    total: number;
+    items: Item[];
+    cliente: { id: number; nombre: string };
+    fechaHora: string;
 };
+
+
+export type Cliente = {
+    id: number;
+    nombre: string;
+    apellido: string;
+    email: string;
+    direccion: string;
+    telefono: string;
+    dni: string;
+    usuario: string;
+    contrasenia: string;
+    fondos: number;
+    favoritos: any[];  // Podés tipar mejor luego
+    pedidos: any[];
+}
