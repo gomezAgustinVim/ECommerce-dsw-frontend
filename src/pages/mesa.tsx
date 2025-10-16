bf6cedbc2536192eb42ce33daa080f79a5e9c514
Tomás <tomaspitavino@gmail.com>
2025-10-16 12:06:06 -0300

    agrego sillas y mesas

---
+++ src/pages/mesa.tsx
@@ -0,0 +1,7 @@
+export default function Mesa() {
+  return (
+    <div className="p-8">
+      <h2 className="text-2xl font-bold text-[#535bf2]">Mesa</h2>
+    </div>
+  );
+}
---
+++ src/pages/sillas.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Sillas() {
+  return (
+    <div className="p-8">
+      <h2 className="text-2xl font-bold text-[#535bf2]">Sillas / Sillones</h2>
+    </div>
+  );
+}
