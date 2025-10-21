b4d7c00fc721250e046c11612921e1311c703c83
lordagustin22 <niconecrofilo589@gmail.com>
2025-10-20 23:39:46 -0300

    Se implementaron todos los tipos

--- src/types.tsx
+++ src/types.tsx
@@ -1,41 +1,73 @@
-export type Mueble = {
-    id: number;
+export type BaseType = {
+  id: number;
+  fechaCreacion: string;       // ISO string (al serializar desde el back)
+  fechaActualizacion: string;  // idem
+};
+
+export type Cliente = BaseType & {
+  nombre: string;
+  apellido: string;
+  direccion: string;
+  telefono: string;
+  dni: string;
+  usuario: string;
+  email: string;
+  contrasenia: string;
+  rol: "user" | "admin";
+  fondos: number;
+  pedidos: Pedido[];
+  favoritos: Favorito[];
+}
+
+export type Categoria = BaseType & {
+    nombre: string;
+    descripcion: string;
+    imagen?: string;
+}
+
+export type Material = BaseType & {
+  nroMaterial: string;
+  nombre: string;
+};
+
+export type Mueble = BaseType & {
     descripcion: string;
     etiqueta: string;
     stock: number;
     precioUnitario: number;
-    imagenes?: string[];
+    imagenes: string[];
+    categoria: Categoria;
+    material: Material;
 };
 
-export type Item = {
-    id: number;
+export type Item = BaseType & {
     cantidad: number;
     subtotal: number;
     estado: string;
     mueble: Mueble;
+    pedido?: Pedido;
 };
 
-export type Pedido = {
-    id: number;
+export type Pedido = BaseType &{
+    cliente: Cliente
+    items: Item[];
+    fechaHora: string;
     estado: string;
     total: number;
-    items: Item[];
-    cliente: { id: number; nombre: string };
-    fechaHora: string;
+    pago?: Pago;
 };
 
+export type Descuento = BaseType & {
+  codigo: string;
+  tipo: 'Cantidad' | 'Monto';
+  descripcion?: string;
+  porcentaje: number;
+  fechaExpiracion: string;
+  pedido: Pedido;
+};
 
-export type Cliente = {
-    id: number;
-    nombre: string;
-    apellido: string;
-    email: string;
-    direccion: string;
-    telefono: string;
-    dni: string;
-    usuario: string;
-    contrasenia: string;
-    fondos: number;
-    favoritos: any[];  // Podés tipar mejor luego
-    pedidos: any[];
-}
+// === FAVORITO ===
+export type Favorito = BaseType & {
+  cliente: Cliente;
+  mueble: Mueble;
+};
