efee98ce1e42477efe92083af9bfc633f0986be3
lordagustin22 <niconecrofilo589@gmail.com>
2026-06-26 14:03:25 -0300

    pagina y tipo pagos

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,40 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Header from './components/header';
-import Login from './pages/login';
-import Carrito from './pages/carrito'; // <-- importar el componente
-import MisPedidos from './pages/misPedidos';
-import Clientes from './pages/clientes';
-import Home from './pages/home';
-import MuebleDetail from './pages/muebleDetail';
-import Footer from './components/footer';
-import Muebles from './pages/muebles';
-import Busqueda from './pages/buscador';
-import Perfil from './pages/perfil';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/header";
+import Login from "./pages/login";
+import Carrito from "./pages/carrito"; // <-- importar el componente
+import MisPedidos from "./pages/misPedidos";
+import Clientes from "./pages/clientes";
+import Home from "./pages/home";
+import MuebleDetail from "./pages/muebleDetail";
+import Footer from "./components/footer";
+import Muebles from "./pages/muebles";
+import Busqueda from "./pages/buscador";
+import Perfil from "./pages/perfil";
+import Pagos from "./pages/pagos.tsx";
 
 function App() {
-    return (
-        <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className='flex-grow bg-gray-100'>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/muebles" element={<Muebles />} /><Route path="/muebles" element={<Muebles />} />
-                    <Route path='/perfil' element={<Perfil />} />
-                    <Route path="/muebles/:id" element={<MuebleDetail />} />
-                    <Route path="/carrito" element={<Carrito />} />{' '}
-                    <Route path="/pedidos" element={<MisPedidos />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/clientes" element={<Clientes />} />
-                    <Route path="/busqueda" element={<Busqueda />} />
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow bg-gray-100">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/muebles" element={<Muebles />} />
+          <Route path="/perfil" element={<Perfil />} />
+          <Route path="/muebles/:id" element={<MuebleDetail />} />
+          <Route path="/carrito" element={<Carrito />} />{" "}
+          <Route path="/pedidos" element={<MisPedidos />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/clientes" element={<Clientes />} />
+          <Route path="/busqueda" element={<Busqueda />} />
+          <Route path="/pagos" element={<Pagos />} />
+        </Routes>
+      </main>
 
-                </Routes>
-            </main>
-
-            <Footer />
-
-        </div>
-    );
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
